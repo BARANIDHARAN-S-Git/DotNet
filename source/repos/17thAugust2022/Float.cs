918fc30bede76c1091034391fb8a712721af2edf
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-08-17 22:19:34 +0530

    17/08/2022

---
+++ source/repos/17thAugust2022/Float.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float radius;
+            Console.WriteLine("Enter the radius");
+            radius=Convert.ToSingle(Console.ReadLine());
+            double area = 3.14 * radius * radius;
+            Console.WriteLine("The area is" + " "+area);
+        }
+    }
+}
