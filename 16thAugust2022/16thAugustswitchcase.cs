b8052e5693fb3c16e9879d24ad0d7bd9a5d0f175
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-08-17 22:42:31 +0530

    16/08/2022

---
+++ 16thAugust2022/16thAugustswitchcase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace consoleApp3;
+
+class switchcase {
+    static void Main(string[] args)
+    {
+        int i=0, j=0, k = 0;
+        int userchoice;
+        char symbol = ' ';
+        Console.WriteLine("enter the Number shown in Menu \n");
+        Console.WriteLine("Menu  : \n 1.Multiply \n 2.Division \n 3.Exit");
+        userchoice=Convert.ToInt32(Console.ReadLine());
+        if (userchoice != 3)
+        {
+            Console.WriteLine("Enter value of i");
+            i=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of j");
+            j = Convert.ToInt32(Console.ReadLine());
+        }
+        switch (userchoice) {
+            case 1:
+                symbol = '+';
+                k = i*j;
+                break;
+            case 2:
+                symbol = '/';
+                k = i/j;
+                break;
+            case 3:
+                Environment.Exit(1);
+                break;
+            default:
+                Environment.Exit(1);
+                break;
+        }
+        Console.WriteLine("The answer of {0} {1} {2} ={3}",i, symbol, j, k);
+        Console.ReadLine();
+
+    }
+}
+
---
+++ 16thAugust2022/16thaugust-dateandtime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleApp2;
+
+class dateandtime {
+    static void Main(string[] args)
+    {
+        Console.Write(DateTime.Now);
+        Console.ReadLine();
+    }
+}
+
