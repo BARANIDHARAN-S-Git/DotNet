40bd5bcc0fc67708115bba715a62b9a0603751b8
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-08-17 22:35:38 +0530

    17/08/2022

---
+++ 17thAugust2022/Findage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime bday;
+            int curryear;
+            int birthyear;
+            Console.WriteLine("enter the name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter the bday");
+            bday = Convert.ToDateTime(Console.ReadLine());
+            birthyear = bday.Year;
+            curryear = DateTime.Now.Year;
+            int age = curryear - birthyear;
+           
+            Console.WriteLine("Hello" +" "+ name +" "+"your age is" +" "+ age);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 17thAugust2022/String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("enter the name");
+            name=Console.ReadLine();
+            Console.WriteLine("Hello"+" "+name);
+            Console.ReadLine();
+        }
+    }
+}
