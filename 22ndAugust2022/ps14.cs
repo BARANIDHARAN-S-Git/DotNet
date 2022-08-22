edd849d17ca88ca9fd2524dde83bd22143e7159f
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-08-22 23:30:57 +0530

    22/08/2022

---
+++ 22ndAugust2022/ps14.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    internal class Program
+    {
+        static int possibleways(int[] S, int m, int n)
+        {
+            if (n == 0)
+                return 1;
+
+
+            if (n < 0)
+                return 0;
+
+            if (m <= 0)
+                return 0;
+
+
+            return possibleways(S, m - 1, n) +
+                possibleways(S, m, n - S[m - 1]);
+        }
+        public static void Main()
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m = ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("s [] = ");
+            int[] s = new int[n];
+            var stringArray = Console.ReadLine().Split(' ');
+            for (var i = 0; i < m; i++)
+            {
+                s[i] = int.Parse(stringArray[i]);
+            }
+            Console.WriteLine("Output = {0}", possibleways(s, m, n));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 22ndAugust2022/ps15.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    internal class ps15
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the n");
+            n=Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int[] a = new int[n];
+            Console.WriteLine("Enter the {0} Elements",n);
+            for(int i = 0; i < n; i++)
+            {
+                a[i] =Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The first repeating element and the idex :");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        count++;
+                    }
+                }
+                
+                if(count > 2 || count==2)
+                {
+                    Console.WriteLine("The Index is :"+(i + 1) +" "+"Elemenet is : " + a[i]);
+                    break;
+
+                }
+                count = 0;
+            }
+
+
+        }
+    }
+}
