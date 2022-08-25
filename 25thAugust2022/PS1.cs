b6a89ac63871719eb5814bbf9b950150c51a77ea
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-08-25 23:47:21 +0530

    25/08/2022

---
+++ 25thAugust2022/PS1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _25thaugust2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string path = @"C:\Users\brother\source\repos\25thaugust2022\";
+                Console.WriteLine("Enter the file name");
+                string fname = Console.ReadLine();
+
+
+                fname = string.Concat(path, fname);
+                Console.WriteLine("Enter any one shown in menu");
+                Console.WriteLine("\nmenu  \n1)create \n2)Add  \n3)truncate \n4)Edit");
+                int input;
+                input = Convert.ToInt32(Console.ReadLine());
+                switch (input) {
+                    case 1:
+                        Console.WriteLine("--------------------------------");  
+
+                        FileStream f = new FileStream(fname, FileMode.Create);
+                        StreamWriter s = new StreamWriter(f);
+
+                        
+                        s.Close();
+                        f.Close();
+                        Console.WriteLine("File created successfully...");
+
+                        break;
+                    case 2:
+                        Console.WriteLine("-----------------");
+                        FileStream f1 = new FileStream(fname, FileMode.Append);
+                        StreamWriter s1 = new StreamWriter(f1);
+
+                        s1.WriteLine("hi this is append");
+                        s1.Close();
+                        f1.Close();
+                        Console.WriteLine(" -----Append  successfully-----");
+
+                        break;
+                    case 3:
+                        Console.WriteLine("-----------------------");
+                        FileStream fs2 = new FileStream(fname, FileMode.Truncate);
+                        StreamWriter ss2 = new StreamWriter(fs2);
+
+                        ss2.WriteLine("hi all this is truncate");
+                        ss2.Close();
+                        fs2.Close();
+                        Console.WriteLine(" ----------Truncate  successfully---------");
+                        break;
+
+
+                    case 4:
+                        Console.WriteLine("-----------------");
+                        FileStream f3 = new FileStream(fname, FileMode.Append);
+                        StreamWriter s3 = new StreamWriter(f3);
+
+                        s3.WriteLine("hi this is Edit");
+                        s3.Close();
+                        f3.Close();
+                        Console.WriteLine(" -----Edit  successfully-----");
+
+                        break;
+                }
+
+
+
+
+
+            }
+        }
+
+
+        }
+    }
+
---
+++ 25thAugust2022/PS2.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25thaugust2022
+{
+    internal class PS2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any one in menu shown below");
+            Console.WriteLine("\nMenu \n1)create a file \n2)Copy that file to a same folder with different filename \n3)Copy that file to a different folder as well \n4)Move the file \n5)Delete a file  \n6)Find the extension of the file \n7) Find the creation date time of the file  \n8)Find the Directory of the file");
+            int input;
+            input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+
+                    string path = @"C:\Users\brother\Desktop\demo\";
+                    Console.WriteLine("Enter the file name");
+                    string fname = Console.ReadLine();
+                    fname = string.Concat(path, fname);
+
+                    File.Create(fname);
+                    Console.WriteLine("File created successfully...");
+                    break;
+
+                case 2:
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("The copy the file to a same folder with differnet file name");
+                    string path1 = @"C:\Users\brother\Desktop\demo\";
+                    Console.WriteLine("Enter the file name");
+                    string fname1 = Console.ReadLine();
+                    fname = string.Concat(path1, fname1);
+                    File.Copy(@"C:\Users\brother\Desktop\demo\new.txt", path1+fname1);
+                    Console.WriteLine("file copied successful");
+                    break;
+
+
+                case 3:
+                    Console.WriteLine("-------------");
+                    Console.WriteLine(" Copy that file to a different folder");
+                    File.Copy(@"C:\Users\brother\Desktop\demo\new.txt", @"C:\Users\brother\Desktop\practice\demo.txt");
+                    Console.WriteLine("file copied to different folder successfully");
+                    break;
+
+
+
+
+
+                case 4:
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("To move the file");
+                    File.Move(@"C:\Users\brother\Desktop\demo\new.txt", @"C:\Users\brother\Desktop\practice\newone.txt");
+                    Console.WriteLine("file moved successfully");
+                    break;
+
+                case 5:
+
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("To delete the file");
+                    string path2 = @"C:\Users\brother\Desktop\demo\";
+                    Console.WriteLine("Enter the file name");
+                    string fname2 = Console.ReadLine();
+                    fname2=String.Concat(path2, fname2);    
+                    File.Delete(fname2);
+                    Console.WriteLine("The file deleted successfully");
+
+                    break;
+
+                case 6:
+
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("  the extension of the file");
+                    string path3 = @"C:\Users\brother\Desktop\demo\";
+                    Console.WriteLine("Enter the file name");
+                    string fname3 = Console.ReadLine();
+                    fname2 = String.Concat(path3, fname3);
+                    FileInfo f=new FileInfo(fname2);
+                    Console.WriteLine("The extension of the file is : " + f.Extension);
+                    break;
+                case 7:
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("  the creation  time of the file");
+                    string path4 = @"C:\Users\brother\Desktop\demo\";
+                    Console.WriteLine("Enter the file name");
+                    string fname4 = Console.ReadLine();
+                    fname2 = String.Concat(path4, fname4);
+                    FileInfo f1 = new FileInfo(fname4);
+                    Console.WriteLine("The creation time is : "+f1.CreationTime);
+
+                    break;
+                case 8:
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("the Directory of the file");
+                    string path5 = @"C:\Users\brother\Desktop\demo\";
+                    Console.WriteLine("Enter the file name");
+                    string fname5 = Console.ReadLine();
+                    fname5 = String.Concat(path5, fname5);
+                    FileInfo f2 = new FileInfo(fname5);
+                    Console.WriteLine("The directory of the filr is : " + f2.Directory);
+
+                    break;
+
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
