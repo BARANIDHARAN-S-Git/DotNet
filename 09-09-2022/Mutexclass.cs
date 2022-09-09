e2198b2d860ca63c8b52e8788d284b8d77a2da56
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-09-09 23:43:47 +0530

    09/09/2022

---
+++ 09-09-2022/Mutexclass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _9thSeptember2022
+{
+    public  class Mutexclass
+    {
+        static void Main(string[] args)
+        {
+            using (Mutex mutex = new Mutex(false, "ThreadingDemo"))
+            {
+
+                if (!mutex.WaitOne(5000, false))
+                {
+                    Console.WriteLine("already running....");
+                    Console.ReadLine();
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine("app is running");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ 09-09-2022/Threadclass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _9thSeptember2022
+{
+    public  class Threadclass
+    {
+        public static void first()
+        {
+           
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(" first={0}", i);
+                if (i == 5)
+                {
+                    Thread.Sleep(1000);
+                }
+
+
+            }
+        }
+
+        public static void second()
+        {
+            
+            
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Second ={0}", j);
+            }
+        }
+
+       
+
+        static void Main(string[] args)
+        {
+          
+
+            Thread t1 = new Thread(first);
+            Thread t2 = new Thread(second);
+            
+          
+
+            t1.Start();
+            t2.Start();
+            
+
+            Console.ReadLine();
+           
+            
+           
+            
+
+        }
+
+       
+
+    }
+}
