872e684c0837ae6182a99e95199e8c00d00baa09
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-09-14 00:54:31 +0530

    14/09/2022

---
+++ 13-09-2022/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using usermodule;
+
+namespace _13thSeptember2022
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Movie m1 = new Movie();
+            m1.AddMovieDetails("Master", "Tamil", "Action", 400);
+            Movie m2 = new Movie();
+            m2.AddMovieDetails("Doctor", "Tamil", "comedy", 75);
+            Movie m3 = new Movie();
+            m3.AddMovieDetails("DJ", "Telugu", "Action", 90);
+            Movie m4 = new Movie();
+            m4.AddMovieDetails("Pushpa", "Telugu", "Thriller", 40);
+            Movie m5 = new Movie();
+            m5.AddMovieDetails("The Gray Man", "English", "Action", 100);
+           
+           
+
+            User u1 = new User();
+            User u2 = new User();
+            User u3 = new User();
+            User u4 = new User();
+            User u5 = new User();
+          
+
+            u2.AddUser("Vel", "Murugan", "Silver");
+            u3.AddUser("Chiranjeevi", "Saravanan", "Gold");
+            u4.AddUser("Prakah", "Murugan", "Platinum");
+            u5.AddUser("Dinesh", "Kumar", "Silver");
+           
+            
+            u2.Movieborrowed(m1);
+            u4.Movieborrowed(m1);
+            Console.WriteLine("Movie 1 Availability " + m1.Available);
+            u2.RetrunMovie(m1, 4);
+            Console.WriteLine("Movie 1 Availability " + m1.Available);
+            u3.Movieborrowed(m1);
+           
+            u5.Movieborrowed(m1);
+          
+            u1.Movieborrowed(m2);
+            u1.RetrunMovie(m2, 5);
+            u1.Movieborrowed(m3);
+            Console.WriteLine("----------------------------------------------------------------");
+            u1.ShowMyList();
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.WriteLine("Based on Language");
+            u1.SearchByLanguage("Tamil");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Based on genre");
+            u1.SearchByGenre("Action");
+            
+
+
+        }
+    }
+
+        
+    
+}
---
+++ 13-09-2022/User.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usermodule
+{
+    public  class User
+    {
+        public int moviesBorrowed = 0;
+        public enum UserLevel
+        {
+            Silver = 2,
+            Gold = 3,
+            Platinum = 5
+        }
+        public List<Movie> BorrowedMovies;
+        public User()
+        {
+            this.userLevel = (int)UserLevel.Silver;
+            BorrowedMovies = new List<Movie>();
+
+        }
+        public User(string UserName, string Password, string UserLevel1)
+        {
+            if (UserLevel1 == "Silver")
+            {
+                this.userLevel = (int)UserLevel.Silver;
+            }
+            if (UserLevel1 == "Gold")
+            {
+                this.userLevel = (int)UserLevel.Gold;
+            }
+            if (UserLevel1 == "Platinum")
+            {
+                this.userLevel = (int)UserLevel.Platinum;
+            }
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+        private int _UserLevel;
+
+        public int userLevel
+        {
+            get { return _UserLevel; }
+            set { _UserLevel = value; }
+        }
+        private string _UserName;
+
+        public string UserName
+        {
+            get { return _UserName; }
+            set { _UserName = value; }
+        }
+        private string _Password;
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; }
+        }
+        public void AddUser(string UserName, string Password, string UserLevel1)
+        {
+            if (UserLevel1 == "Silver")
+            {
+                this.userLevel = (int)UserLevel.Silver;
+            }
+            if (UserLevel1 == "Gold")
+            {
+                this.userLevel = (int)UserLevel.Gold;
+            }
+            if (UserLevel1 == "Platinum")
+            {
+                this.userLevel = (int)UserLevel.Platinum;
+            }
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+        public void Movieborrowed(Movie mov)
+        {
+            if (mov.Available > 0)
+            {
+                if (this.moviesBorrowed <= this.userLevel)
+                {
+                    mov.Available--;
+                    this.moviesBorrowed++;
+                    BorrowedMovies.Add(mov);
+                }
+                else
+                {
+                    Console.WriteLine("cannot able to borrow movies");
+                }
+            }
+            else
+            {
+                Console.WriteLine("mivie is not available");
+            }
+        }
+        public void RetrunMovie(Movie m, int days)
+        {
+            if (this.BorrowedMovies.Contains(m))
+            {
+
+                m.Available++;
+                this.moviesBorrowed--;
+                BorrowedMovies.Remove(m);
+                double totalCost = days * m.Cost;
+                Console.WriteLine($"You Need to Pay {totalCost}");
+            }
+            else
+            {
+                Console.WriteLine("The Borrowed movie and returned movie is not same");
+            }
+        }
+        public void SearchByLanguage(string language)
+        {
+            foreach (var item in Movie.l)
+            {
+                if (item.Language == language)
+                {
+                    Console.WriteLine($"Movie Name : {item.MovieName}  Language : {item.Language} Genere : {item.genres}");
+                }
+            }
+        }
+        public void SearchByGenre(string genre)
+        {
+            foreach (var item in Movie.l)
+            {
+                if (item.genres == genre)
+                {
+                    Console.WriteLine($"Movie Name : {item.MovieName}  Language : {item.Language} Genere : {item.genres}");
+                }
+            }
+        }
+        public void ShowMyList()
+        {
+            foreach (var item in this.BorrowedMovies)
+            {
+                Console.WriteLine($"Movie Name : {item.MovieName}  Language : {item.Language} Genere : {item.genres}");
+            }
+        }
+    }
+}
