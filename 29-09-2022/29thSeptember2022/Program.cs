262bf9f3a78202ea85454a2c6ee528bb909e1d37
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-09-29 23:57:55 +0530

    29/09/2022

---
+++ 29-09-2022/29thSeptember2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using static DataAccessLibrary.BankDomain;
+
+namespace _29thSeptember2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+            context.BorrowerTable.Add(new Borrower { borrowerid = 1, borr_name = "Buttler", City = "Chennai", state = "Tamilnadu", country = "India", street = "19,AJF Garden", companyname = "Mphasis", jobname = "Software Engineer" });
+            context.LoanTable.Add(new Loan { LoanID = 1, LoanType = "EducationLoan", Amount = 100000, duration = "2years", RateOfInterest =" 2.7 % p.a" });
+            context.SaveChanges();
+            Console.WriteLine("done");
+            Console.Read();
+        }
+    }
+}
---
+++ 29-09-2022/DataAccessLibrary/BankDomain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+
+    public class BankDomain
+    {
+
+        public class Borrower
+        {
+            [Key]
+            [Required]
+            public int borrowerid { get; set; }
+
+            [MaxLength(30, ErrorMessage = "Not allowed above 20 chars")]
+            [MinLength(3, ErrorMessage = "Not allowed below 2 chars")]
+            public string borr_name { get; set; }
+
+            public string City { get; set; }
+
+            public string state { get; set; }
+
+            public string country { get; set; }
+
+            public string street { get; set; }
+
+            public string companyname { get; set; }
+
+            public string jobname { get; set; }
+
+        }
+
+        public class Loan
+        {
+            [Key]
+            public int LoanID { get; set; }
+
+            public string LoanType { get; set; }
+
+            [Range(10000, 200000, ErrorMessage = "not in range")]
+            public int Amount { get; set; }
+
+            public string duration { get; set; }
+
+            public string RateOfInterest { get; set; }
+
+        }
+
+
+
+        public class MyContext : DbContext
+        {
+            public MyContext() : base("MyContext")
+            {
+
+                Database.SetInitializer<MyContext>(new CreateDatabaseIfNotExists<MyContext>());
+            }
+
+            public virtual DbSet<Borrower> BorrowerTable { get; set; }
+            public virtual DbSet<Loan> LoanTable { get; set; }
+
+        }
+
+    }
+  
+
+}
