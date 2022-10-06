7d8b6ecb3355c5a2202c84e7604b88765fbf93f8
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-10-07 00:16:20 +0530

    04/10/2022

---
+++ 04-10-2022/4thOctober2022/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4thOctober2022.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+        public ActionResult DeleteCustomer()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 04-10-2022/4thOctober2022/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4thOctober2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return Redirect("Index");
+        }
+        public ActionResult Details()
+        {
+            string content = "Hi,This is Buttler,I am from England";
+            return Content(content);
+        }
+    }
+}
